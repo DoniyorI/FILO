0c91fb5d5cd081e8dfa07df4613e63e8039f8c5f
SoloH3N <phunlian@buffalo.edu>
2023-10-09 16:34:17 -0400

    installed tailwind and flask dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  /* content reads in the css classes from the index.html file */ 
+  content: ["./src/*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
