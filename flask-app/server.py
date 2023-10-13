a0d0991fa0b12e3e63da4fe4db441c0605981002
JimmyWu7 <104340474+JimmyWu7@users.noreply.github.com>
2023-10-12 22:21:01 -0400

    React + Flask

---
+++ flask-app/server.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, Response, send_file, make_response, send_from_directory
+
+app = Flask(__name__)
+
+# Define a route to serve the index.html file
+@app.route('/')
+def serve_react_app():
+
+    try:
+        response = make_response(send_file('../my-react-app/build/index.html', mimetype='text/html'))
+        response.headers['X-Content-Type-Options'] = 'nosniff'
+        return response
+    except Exception:
+        return page_not_found()
+
+@app.route('/static/css/<path:filename>')
+def serve_static_css(filename):
+    try:
+        response = send_from_directory('../my-react-app/build/static/css', filename)
+        response.headers['MIME type'] = 'text/css'
+        response.headers['X-Content-Type-Options'] = 'nosniff'
+        return response
+    except Exception:
+        return page_not_found()
+
+
+@app.route('/static/js/<path:filename>')
+def serve_static_js(filename):
+    try:
+        response = send_from_directory('../my-react-app/build/static/js', filename)
+        response.headers['MIME type'] = 'text/javascript'
+        response.headers['X-Content-Type-Options'] = 'nosniff'
+        return response
+    except Exception:
+        return page_not_found()
+
+
+@app.errorhandler(404)
+def page_not_found(error=None):
+    return Response(
+        response='HTTP/1.1 404 Not Found',
+        status=404,
+        mimetype="text/plain",
+        headers={'X-Content-Type-Options': 'nosniff'}
+    )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
