806804b3eaf18bd80254f98f63861cfaed4a5f04
JimmyWu7 <104340474+JimmyWu7@users.noreply.github.com>
2023-10-21 13:18:47 -0400

    New Post Update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,12 +6,15 @@ import Posts from "./Posts";
 /* create const variables for reading in user get requests to make posts live on the home page */
 const Home = () => {
   return (
-    <div className="home_bg ml-20"> {/* Adjust the left margin here */}
+    <div className="home_bg h-screen ml-20"> {/* Adjust the left margin here */}
       <div className="flex justify-center items-center text-3xl text-white pt-2">
-        Home | Following
-      </div>
-      <div className="text-5xl text-white">
-        Helooooooooooooo
+        <a href="/home" className="px-4 text-base">
+          Home
+        </a>
+        |
+        <a href="/following" className="px-4 text-base">
+          Following
+        </a>
       </div>
       <Posts/>
     </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,29 @@
 import React from "react";
+// import { BsPlus } from 'react-icons/bs';
 
 const Sidebar = () => {
   return (
-    <aside className="sidebar_bg h-screen w-20 fixed z-10">
-      <div className="flex flex-col items-center">
-        <div className="h-14 w-14 bg-[#225374] rounded-full mt-6 mb-5"></div>
-        <div className="h-14 w-14 bg-[#225374] rounded-full mb-5"></div>
+    <header>
+      <div class="sidebar_bg fixed w-20 h-screen flex justify-center hover:shadow-inner">
+        <div class="relative flex h-14 w-14 transform items-center justify-center rounded-full bg-blue-400 transition-transform hover:scale-110 mt-4">
+          <div class="absolute left-20 rounded-lg bg-white p-2 text-lg shadow-md transition-opacity opacity-100 hover:block">
+            Surprise!
+          </div>
+        </div>
       </div>
-    </aside>
+
+      {/* <SidebarIcon icon={<BsPlus size="32" />}/> */}
+    </header>
   );
 };
 
+// const SidebarIcon = ({ icon }) => (
+//   <div className="sidebar-icon group">
+//     {icon}
+//     <span class="group-hover:scale-100">
+//       Hello
+//     </span>
+//   </div>
+// )
 
 export default Sidebar;
