f28192994f1ebe17b06552f264ab189497e037b3
SoloH3N <phunlian@buffalo.edu>
2023-10-22 19:17:04 -0400

    added to Posts.jsx

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -8,11 +8,61 @@ const Posts = () => {
   const [error, setError] = useState(null);
 
   const [clicked, setClicked] = useState([]);
+  const [formData, setFormData] = useState({
+    like_counts: ""
+    });
+    const { like_counts } = formData;
+    const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+    ...formData,
+    [name]: value,
+    });
+    };
+    
+    const handleLikes = async (event) => {
+    event.preventDefault();
+    // Create an object with the data you want to send to the server
+    
+      const dataToSend = {
+      
+      likes: like_counts
+      
+      };
+      
+      console.log(dataToSend);
+      
+        
+      
+      try {
+      
+      const response = await fetch("/post-like", {
+      method: "POST",
+      headers: {
+      "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dataToSend),
+      });
+    
+      if (response.ok) {
+      console.log(dataToSend);
+      window.location.href = "/";
+      // Registration was successful, you can handle the response here
+      // e.g., redirect or display a success message
+      } else {
+      // Registration failed, handle the error here
+      // e.g., display an error message
+      }
+      } catch (error) {
+      // ...
+      }
+    }
 
   function handleClick(index) {
     const newClicked = [...clicked];
     newClicked[index] = !newClicked[index]; // Toggle the clicked state for the specific post
     setClicked(newClicked);
+    handleLikes();
   }
 
   useEffect(() => {
