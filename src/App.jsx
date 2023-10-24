fab952fd1bde0ed2c86e01bd11739ee027dabf25
JimmyWu7 <104340474+JimmyWu7@users.noreply.github.com>
2023-10-24 15:40:56 -0400

    Fixed Loading Page Error

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import BarLoader from "react-spinners/BarLoader";
 
-// import Loading from "./components/Loading";
 import Register from "./components/Register";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Nav";
@@ -11,10 +10,17 @@ import "./index.css";
 
 export default function App() {
   const [loading, setLoading] = useState(true);
+  const [registerLoading, setRegisterLoading] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
+    }, 5000);
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setRegisterLoading(false);
     }, 5000);
   }, []);
 
@@ -25,12 +31,17 @@ export default function App() {
           path="/"
           element={
             <>
-              <div className="flex h-screen justify-center items-center">
-                <BarLoader color={"#EFA73E"} loading={loading} size={30} />
-              </div>
-              <Sidebar />
-              <Navbar />
-              <Home />
+              {loading ? (
+                <div className="flex h-screen justify-center items-center">
+                  <BarLoader color={"#EFA73E"} loading={loading} size={30} />
+                </div>
+              ) : (
+                <>
+                  <Sidebar />
+                  <Navbar />
+                  <Home />
+                </>
+              )}
             </>
           }
         />
@@ -38,10 +49,17 @@ export default function App() {
           path="login"
           element={
             <>
-              <div className="flex h-screen justify-center items-center">
-                <BarLoader color={"#EFA73E"} loading={loading} size={30} />
-              </div>
-              <Register />
+              {registerLoading ? (
+                <div className="flex h-screen justify-center items-center">
+                  <BarLoader
+                    color={"#EFA73E"}
+                    loading={registerLoading}
+                    size={30}
+                  />
+                </div>
+              ) : (
+                <Register />
+              )}
             </>
           }
         />
