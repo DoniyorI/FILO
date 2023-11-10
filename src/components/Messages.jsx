ea32bb870a7291ef7373b07c7f44be3adf2fd3ef
Doniyor <doniyori@buffalo.edu>
2023-11-10 00:39:35 -0500

    Channel Message page mostly complete

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
+
 import { IoSend } from "react-icons/io5";
 // import SendIcon from "../assets/sendIcon.svg";
 import SidebarMsg from "../components/SidebarMsg";
@@ -6,14 +7,41 @@ import { BsInfoCircle } from "react-icons/bs";
 
 import UserContext from "./UserContext";
 
-const members = [
-  { id: 1, name: "User 1" },
-  { id: 2, name: "User 2" },
-  { id: 3, name: "User 3" },
-];
-
 function Messages() {
   const { user, dmUsers, channels } = useContext(UserContext);
+
+  const [expanded, setExpanded] = useState(false);
+
+  // Mock data for members
+  const members = [
+    "User 1",
+    "User 2",
+    "User 3",
+    "User 4",
+    "User 5",
+    "User 6",
+    "User 7",
+    "User 8",
+    "User 9",
+    "User 10",
+    "User 11",
+    "User 1",
+    "User 2",
+    "User 3",
+    "User 4",
+    "User 5",
+    "User 6",
+    "User 7",
+    "User 8",
+    "User 9",
+    "User 10",
+    "User 11",
+  ];
+
+  // Function to handle expanding the description
+  const toggleExpand = () => {
+    setExpanded(!expanded);
+  };
 
   // how do I get the data from the user (_id) database? useEffect?
 
@@ -36,23 +64,77 @@ function Messages() {
         </div>
 
         {/* Side bar on right for channel information */}
-        <div className="bg-sand w-[15%] fixed mt-14 right-0 h-full z-20">
-          {/* Sidebar content goes here */}
-        </div>
-          {/* Scrollable Messages Section */}
-          <div className="overflow-y-auto mr-[17%]">
-            {/* Sample Message */}
-            <div className="flex items-start p-4 "> {/* Changed from items-center to items-start */}
-              <div className="flex-shrink-0 bg-sand w-11 h-11 border-[1px] border-goldenOrange rounded-full mr-3">
-                {/* Sender Icon */}
-              </div>
-              <div className="flex-1 min-w-0">
-                <p className="text-sm font-semibold truncate text-white">Sender Name</p>
-                <p className="text-md break-words text-white">This is a very long sample message that might cause issues if it's too long for the container, but now it should wrap or truncate properly.</p>
+        <div className="nav_bg w-[15%] fixed mt-14 right-0 h-full z-20 overflow-hidden ">
+          <div className="p-4">
+            <div className="text-white">
+              <h2 className="font-bold">Description:</h2>
+              <p
+                className={`text-sm ${
+                  expanded ? "line-clamp-none" : "line-clamp-5"
+                }`}
+              >
+                Lorem ipsum dolor sit amet, no nam vidit noluisse theophrastus,
+                veri accusan adolescens mei in, offendit...Lorem ipsum dolor sit
+                amet, no nam vidit noluisse theophrastus, veri accusan
+                adolescens mei in, offendit...Lorem ipsum dolor sit amet, no nam
+                vidit noluisse theophrastus, veri accusan adolescens mei in,
+                offendit...
+                {!expanded && (
+                  <span
+                    className="text-blue-600 cursor-pointer"
+                    onClick={toggleExpand}
+                  >
+                    ... (more)
+                  </span>
+                )}
+              </p>
+            </div>
+
+            <div className="mt-4 text-white">
+              <h2 className="text-xl font-bold py-5">
+                Members: ...LIVE COUNTER{" "}
+              </h2>
+              <div className="overflow-auto h-[40vh]">
+                {members.map((member, index) => (
+                  <div key={index} className="flex items-center">
+                    <div className="bg-primaryBlue w-11 h-11 border-[1px] border-goldenOrange rounded-full m-2"></div>
+                    <div className="text-lg text-white">{member}</div>
+                  </div>
+                ))}
               </div>
             </div>
-            {/* More messages can be added here */}
           </div>
+
+          <div className="flex flex-col justify-center items-center w-full text-center">
+            <p className="text-xl z-30 text-bold text-white">Remaining Session Time: </p>
+            <div className="w-[80%] p-2 bg-goldenOrange text-white">
+              <p className="text-lg ">TIME</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Scrollable Messages Section */}
+        <div className="overflow-y-auto mr-[17%]">
+          {/* Sample Message */}
+          <div className="flex items-start p-4 ">
+            {" "}
+            {/* Changed from items-center to items-start */}
+            <div className="flex-shrink-0 bg-sand w-11 h-11 border-[1px] border-goldenOrange rounded-full mr-3">
+              {/* Sender Icon */}
+            </div>
+            <div className="flex-1 min-w-0">
+              <p className="text-sm font-semibold truncate text-white">
+                Sender Name
+              </p>
+              <p className="text-md break-words text-white">
+                This is a very long sample message that might cause issues if
+                it's too long for the container, but now it should wrap or
+                truncate properly.
+              </p>
+            </div>
+          </div>
+          {/* More messages can be added here */}
+        </div>
       </section>
 
       {/* Fixed Message Input Section */}
@@ -61,19 +143,18 @@ function Messages() {
           <form className="bg-white rounded-2xl ">
             <input
               placeholder="Enter a Message"
-              className="w-full rounded-2xl px-2 pr-8"
+              className="w-full rounded-2xl px-3  pr-8 py-1"
             />
             <button
               type="submit"
-              className="absolute right-2 bottom-1 hover:scale-110"
+              className="absolute right-2 bottom-1 hover:scale-110 py-1"
             >
-              <IoSend width={10} height={10} className="text-primaryDark" />
+              <IoSend height={14} className="text-primaryDark" />
             </button>
           </form>
         </div>
       </div>
     </>
-
   );
 }
 
--- src/components/SidebarMsg.jsx
+++ src/components/SidebarMsg.jsx
@@ -8,24 +8,26 @@ const SidebarMsg = () => {
   return (
     <>
       <div className="w-1/6 h-screen sidebar_bg z-10 fixed">
-        <div className="m-2 flex items-center mb-2">
-          <a href="/" className="">
-            <img src={Logo} alt="logo" width={50} />
+        <div className="">
+          <a href="/" className="m-2 flex items-center">
+            <img src={Logo} alt="logo" width={50} className="mx-2"/>
+            <img src={FILO} width={80}  />
+
           </a>
-          <a href="/" className="pl-6">
-            <img src={FILO} width={70} height={70} />
-          </a>
+          {/* <a href="/" className="pl-6">
+            <img src={FILO} width={80}  />
+          </a> */}
         </div>
 
-        <div className="flex pl-4 pt-4">
+        <div className="flex pl-4 py-2">
           <div className="w-12 h-12 rounded-full bg-primaryBlue border-[1px] border-goldenOrange"></div>
           <h1 className="flex items-center pl-4 text-md">Jimmy</h1>
         </div>
         <div>
-          <hr class="w-[14rem] h-[1px] mx-auto mb-4 mt-20 bg-black border-0 rounded" />
+          <hr class="w-[80%] h-[1px] mx-auto my-4 bg-black border-0 rounded" />
         </div>
         <div>
-          <div className="flex justify-center items-center">
+          <div className="flex pl-4 py-2">
             <div className="w-[13rem] h-12 rounded-3xl bg-sand flex">
               <div className="flex bg-goldenOrange w-12 h-full rounded-full justify-center items-center text-3xl text-sand">
                 #
